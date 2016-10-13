cdc66d8f7fe30e79212ba368ab61025dc514606e
Xinke Chen <xinkecf35@gmail.com>
2016-10-13 00:35:04 -0400

    Removed Redundancy from file

--- TrafficAlarmClock/SettingsViewController.h
+++ TrafficAlarmClock/SettingsViewController.h
@@ -8,7 +8,7 @@
 #import <UIKit/UIKit.h>
 #import "Foundation/Foundation.h"
 
-@interface SettingsViewController : UITableViewController < UITableViewDataSource, UITableViewDelegate>
+@interface SettingsViewController : UITableViewController 
 
 
 @end
