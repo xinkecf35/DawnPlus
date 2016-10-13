0ab6e4b809c2acd3b9ef280f55f0f5d06f43c42d
Xinke Chen <xinkecf35@gmail.com>
2016-10-13 00:29:37 -0400

    Basic UITable Settings have been populated, may proceed further in tweaking different layouts for settings view.

--- TrafficAlarmClock/SettingsViewController.m
+++ TrafficAlarmClock/SettingsViewController.m
@@ -8,19 +8,40 @@
 
 #import "SettingsViewController.h"
 
-@implementation SettingsViewController
+@implementation SettingsViewController 
 {
     NSArray *settings;
 }
 -(void)viewDidLoad
 {
     [super viewDidLoad];
+    settings = [NSArray arrayWithObjects:@"Alarm",@"Traffic",@"Weather",@"About", nil];
 }
 - (void)viewDidUnload
 {
     [super viewDidUnload];
 }
 //UITableView methods
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [settings count];
+}
 
+-(UITableViewCell *)tableView: (UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"SettingsCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier: simpleTableIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    cell.textLabel.text = [settings objectAtIndex:indexPath.row];
+    return cell;
+}
 
 @end
