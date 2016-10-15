461c74c870ea6e5ae12d56e7658721a56af1475b
Xinke Chen <xinkecf35@gmail.com>
2016-10-15 00:34:46 -0400

    Quick Commit, layout settings table view

--- TrafficAlarmClock/SettingsViewController.h
+++ TrafficAlarmClock/SettingsViewController.h
@@ -10,7 +10,7 @@
 
 @interface SettingsViewController : UITableViewController
 {
-    NSArray *settings;
+    //NSArray *settings;
 }
 
 
--- TrafficAlarmClock/SettingsViewController.m
+++ TrafficAlarmClock/SettingsViewController.m
@@ -12,33 +12,33 @@
 -(void)viewDidLoad
 {
     [super viewDidLoad];
-    settings = [NSArray arrayWithObjects:@"Alarm",@"Traffic",@"Weather",@"About", nil];
+    //settings = [NSArray arrayWithObjects:@"Alarm",@"Traffic",@"Weather",@"About", nil];
 }
 - (void)viewDidUnload
 {
     [super viewDidUnload];
 }
 //UITableView methods
--(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
-    return 1;
-}
--(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-    return [settings count];
-}
-
--(UITableViewCell *)tableView: (UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    static NSString *simpleTableIdentifier = @"SettingsCell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier: simpleTableIdentifier];
-    
-    if (cell == nil) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
-    }
-    
-    cell.textLabel.text = [settings objectAtIndex:indexPath.row];
-    return cell;
-}
+//-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+//{
+//    return 1;
+//}
+//-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+//{
+//    return [settings count];
+//}
+//
+//-(UITableViewCell *)tableView: (UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+//{
+//    static NSString *simpleTableIdentifier = @"SettingsCell";
+//    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier: simpleTableIdentifier];
+//    
+//    if (cell == nil) {
+//        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+//    }
+//    
+//    cell.textLabel.text = [settings objectAtIndex:indexPath.row];
+//    return cell;
+//}
 
 @end
