6d82c172f027f3ce0645ccac2f84cf1c66794023
Xinke Chen <xinkecf35@gmail.com>
2016-11-19 00:05:04 -0500

    Started work on UnitsViewController

--- TrafficAlarmClock/SwiftClasses/UnitsViewController.swift
+++ TrafficAlarmClock/SwiftClasses/UnitsViewController.swift
@@ -10,12 +10,34 @@ import Foundation
 import UIKit
 
 class UnitsViewController: UITableViewController {
+    let unitOptions : [String] = ["Fahrenheit","Celsius"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
-        let rowToSelect:IndexPath = IndexPath(row: 1, section: 0)
+        let rowToSelect:IndexPath = IndexPath(row: 0, section: 0)
         tableView.cellForRow(at: rowToSelect)?.accessoryType = UITableViewCellAccessoryType.checkmark
+    }
+    //Delegate and Datasource Methods
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return unitOptions.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "unitsCell", for: indexPath)
+        
+        cell.textLabel?.text = unitOptions[indexPath.row]
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        if(tableView.cellForRow(at: indexPath)?.accessoryType == UITableViewCellAccessoryType.none) {
+            tableView.cellForRow(at: indexPath)?.accessoryType = UITableViewCellAccessoryType.checkmark
+        }
+        else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = UITableViewCellAccessoryType.none
+        }
     }
 }
