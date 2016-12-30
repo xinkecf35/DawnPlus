d62a3b2a25462db7f35487ce9f0a3730e9573be6
Xinke Chen <xinkecf35@gmail.com>
2016-12-30 05:59:00 -0500

    Fixed boilerplate and UserDefault forKey to be specific to view controller only

--- TrafficAlarmClock/SwiftClasses/SensitivityViewController.swift
+++ TrafficAlarmClock/SwiftClasses/SensitivityViewController.swift
@@ -12,7 +12,7 @@ import UIKit
 class SensitivityViewController: UITableViewController {
     //Constants and Default values
     let trafficOptions : [String] = ["Incidents","Events","Congestion","Construction"]
-    let checkedCellsConstant : String = "checkedCells"
+    let checkedCellsConstant : String = "sensitivityCheckedCells"
     let defaults : UserDefaults = UserDefaults.standard
     var checkedCells: [Int] = [0,0,0,0]
     
@@ -28,8 +28,9 @@ class SensitivityViewController: UITableViewController {
         
     }
     override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(<#T##animated: Bool##Bool#>)
+        super.viewWillDisappear(true)
         defaults.set(checkedCells, forKey: checkedCellsConstant)
+        NSLog("Defaults for %@ saved", self)
     }
     //Delegate and Datasource Methods
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -47,6 +48,8 @@ class SensitivityViewController: UITableViewController {
     
     //Multiple Selection
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        tableView.deselectRow(at: indexPath, animated: false)
+        
         if(tableView.cellForRow(at: indexPath)?.accessoryType == UITableViewCellAccessoryType.none) {
             tableView.cellForRow(at: indexPath)?.accessoryType = UITableViewCellAccessoryType.checkmark
             checkedCells[indexPath.row] = 1
