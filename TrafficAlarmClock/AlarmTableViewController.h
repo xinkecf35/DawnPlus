47801b7fa546a1f159c557554cd9583578e1d0a4
Xinke Chen <xinkecf35@gmail.com>
2017-05-17 02:58:40 -0400

    Started work on AlarmTableViewController

---
+++ TrafficAlarmClock/AlarmTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  AlarmTableViewController.h
+//  TrafficAlarmClock
+//
+//  Created by Xinke Chen on 2017-05-17.
+//  Copyright © 2017 Xinke Chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+@import UIKit;
+
+@interface AlarmTableViewController : UITableViewController
+
+
+@end
---
+++ TrafficAlarmClock/AlarmTableViewController.m
@@ -0,0 +1,21 @@
+//
+//  AlarmTableViewController.m
+//  TrafficAlarmClock
+//
+//  Created by Xinke Chen on 2017-05-17.
+//  Copyright © 2017 Xinke Chen. All rights reserved.
+//
+
+#import "AlarmTableViewController.h"
+
+@implementation AlarmTableViewController
+
+-(void)viewDidLoad {
+    
+}
+
+-(void)viewDidUnload {
+    
+}
+
+@end
