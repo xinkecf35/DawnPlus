e2930d6a799b00d9ebc54469162e985965143776
Xinke Chen <xinkecf35@gmail.com>
2017-05-19 00:45:24 -0400

    Started adding Core Data to project for data persistence

--- TrafficAlarmClock/AlarmObject.m
+++ TrafficAlarmClock/AlarmObject.m
@@ -9,28 +9,12 @@
 #import "AlarmObject.h"
 @implementation AlarmObject
 
-@synthesize alarmTime,enabled,notificationID,gentleWake;
+@dynamic dayToRepeat, alarmTime,enabled,notificationID,gentleWake;
 
 //This is important to for saving the alarm object in user defaults
 -(void)setAlarm;
 {
     
 }
-
--(void)encodeWithCoder:(NSCoder *)encoder
-{
-    [encoder encodeObject:self.alarmTime forKey:@"alarmTime"];
-    [encoder encodeBool:self.enabled forKey:@"enabled"];
-    [encoder encodeInt:self.notificationID forKey:@"notificationID"];
-}
-//This is important to for loading the alarm object from user defaults
--(id)initWithCoder:(NSCoder *)decoder
-{
-    self.alarmTime = [decoder decodeObjectForKey:@"timeToSetOff"];
-    self.enabled = [decoder decodeBoolForKey:@"enabled"];
-    self.notificationID = [decoder decodeIntForKey:@"notificationID"];
-    return self;
-}
-
 
 @end
