0f36577d96bf58c8ab077835de4d636b6f41c2fd
Xinke Chen <xinkecf35@gmail.com>
2017-05-21 03:36:14 -0400

    Added save method to Core Data controller

--- TrafficAlarmClock/CoreDataController.m
+++ TrafficAlarmClock/CoreDataController.m
@@ -29,9 +29,53 @@
 
 -(void)initializeCoreData {
     if([self managedObjectContext]) return;
+    
     NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"DataModel" withExtension:@"momd"];
     NSManagedObjectModel *mom = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
     NSAssert(mom, @"%@:%@ No model to generate a store from", [self class], NSStringFromSelector(_cmd));
+    
+    NSPersistentStoreCoordinator *coordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:mom];
+    NSAssert(coordinator, @"Failed to initialize coordinator");
+    
+    [self setManagedObjectContext:[[NSManagedObjectContext alloc] initWithConcurrencyType:NSMainQueueConcurrencyType]];
+    [self setPrivateContext:[[NSManagedObjectContext alloc] initWithConcurrencyType:NSPrivateQueueConcurrencyType]];
+    [[self privateContext] setPersistentStoreCoordinator:coordinator];
+    [[self managedObjectContext] setParentContext:[self privateContext]];
+    //Analyse the following block of code; for now this is going to black magic
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
+        NSPersistentStoreCoordinator *psc = [[self privateContext] persistentStoreCoordinator];
+        NSMutableDictionary *options = [NSMutableDictionary dictionary];
+        options[NSMigratePersistentStoresAutomaticallyOption] = @YES;
+        options[NSInferMappingModelAutomaticallyOption] = @YES;
+        options[NSSQLitePragmasOption] = @{ @"journal_mode":@"DELETE" };
+        
+        NSFileManager *fileManager = [NSFileManager defaultManager];
+        NSURL *documentsURL = [[fileManager URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
+        NSURL *storeURL = [documentsURL URLByAppendingPathComponent:@"DataModel.sqlite"];
+        
+        NSError *error = nil;
+        NSAssert([psc addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:options error:&error], @"Error initializing PSC: %@\n%@", [error localizedDescription], [error userInfo]);
+        if (![self callback]) return;
+        
+        dispatch_sync(dispatch_get_main_queue(), ^{
+            [self callback]();
+        });
+    });
+}
+
+-(void)save {
+    if (![[self privateContext] hasChanges] && ![[self managedObjectContext] hasChanges]) return;
+    
+    [[self managedObjectContext] performBlockAndWait:^{
+        NSError *error = nil;
+        
+        NSAssert([[self managedObjectContext] save:&error], @"Failed to save main context: %@\n%@", [error localizedDescription], [error userInfo]);
+        
+        [[self privateContext] performBlock:^{
+            NSError *privateError = nil;
+            NSAssert([[self privateContext] save:&privateError], @"Error saving private context: %@\n%@", [privateError localizedDescription], [privateError userInfo]);
+        }];
+    }];
 }
 
 @end
