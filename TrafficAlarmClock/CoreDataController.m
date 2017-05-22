118576f532342fe9c46be7d519974dc40ad7f64b
Xinke Chen <xinkecf35@gmail.com>
2017-05-21 20:12:07 -0400

    Some naming modifications

--- TrafficAlarmClock/CoreDataController.m
+++ TrafficAlarmClock/CoreDataController.m
@@ -30,7 +30,7 @@
 -(void)initializeCoreData {
     if([self managedObjectContext]) return;
     
-    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"TrafficAlarmClock" withExtension:@"momd"];
+    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"DawnPlusDataModel" withExtension:@"momd"];
     NSManagedObjectModel *mom = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
     NSAssert(mom, @"%@:%@ No model to generate a store from", [self class], NSStringFromSelector(_cmd));
     
@@ -51,7 +51,7 @@
         
         NSFileManager *fileManager = [NSFileManager defaultManager];
         NSURL *documentsURL = [[fileManager URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
-        NSURL *storeURL = [documentsURL URLByAppendingPathComponent:@"DataModel.sqlite"];
+        NSURL *storeURL = [documentsURL URLByAppendingPathComponent:@"DawnPlusDataModel.sqlite"];
         
         NSError *error = nil;
         NSAssert([psc addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:options error:&error], @"Error initializing PSC: %@\n%@", [error localizedDescription], [error userInfo]);
