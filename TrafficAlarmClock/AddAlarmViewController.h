aec4e2132f3d53b1c133e10363c1ea06b6165ebb
Xinke Chen <xinkecf35@gmail.com>
2017-06-02 17:32:08 -0400

    Started work on ViewController to add alarms

---
+++ TrafficAlarmClock/AddAlarmViewController.h
@@ -0,0 +1,19 @@
+//
+//  AddAlarmViewController.h
+//  TrafficAlarmClock
+//
+//  Created by Xinke Chen on 2017-06-02.
+//  Copyright © 2017 Xinke Chen. All rights reserved.
+//
+
+#import "Foundation/Foundation.h"
+#import <UIKit/UIKit.h>
+#import "AlarmObject.h"
+
+@interface AddAlarmViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIDatePicker *timePicker;
+
+
+@end
+
