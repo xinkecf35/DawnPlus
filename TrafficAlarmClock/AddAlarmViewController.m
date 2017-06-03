b6f8a77584f810849aeb743490da96988c27ddb0
Xinke Chen <xinkecf35@gmail.com>
2017-06-02 21:02:27 -0400

    Started implementations for AddAlarmViewController

---
+++ TrafficAlarmClock/AddAlarmViewController.m
@@ -0,0 +1,18 @@
+//
+//  AddAlarmViewController.m
+//  TrafficAlarmClock
+//
+//  Created by Xinke Chen on 2017-06-02.
+//  Copyright © 2017 Xinke Chen. All rights reserved.
+//
+
+#import "AddAlarmViewController.h"
+
+@implementation AddAlarmViewController
+
+-(void) viewDidLoad {
+    
+}
+
+
+@end
