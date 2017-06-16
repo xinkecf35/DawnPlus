9b35dc9e84b34a811c31751e6821e974b389ff79
Xinke Chen <xinkecf35@gmail.com>
2017-06-16 19:39:54 -0400

    Added UIAlertController and Cleaned up files

---
+++ DawnPlus/DawnPlus-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  DawnPlus-Bridging-Header.h
+//  DawnPlus
+//
+//  Created by Xinke Chen on 2017-06-16.
+//  Copyright © 2017 Xinke Chen. All rights reserved.
+//
+
+#import "TrafficFetch.h"
+#import "GeocodeFetch.h"
+#import "LocationFetch.h"
