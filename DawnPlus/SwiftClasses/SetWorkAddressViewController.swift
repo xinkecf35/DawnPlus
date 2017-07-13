20fd13d5447aa0b94874ba89a00bcd7034004138
Xinke Chen <xinkecf35@gmail.com>
2017-07-13 01:16:55 -0400

    Rewriting Set Address Viewcontroller to programatically layout views

--- DawnPlus/SwiftClasses/SetWorkAddressViewController.swift
+++ DawnPlus/SwiftClasses/SetWorkAddressViewController.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+import UIKit
+import MapKit
+
+class SetWorkAddressViewController:UIViewController {
+    let defaults:UserDefaults = UserDefaults.standard
+    let resultsController = AddressResultsViewController()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        LocationFetch.addObserver(self, forKeyPath: #keyPath(LocationFetch.currentLocation), options: NSKeyValueObservingOptions.new, context: nil)
+    }
+    override func viewDidLoad() {
+        let mapView = MKMapView()
+        let span = MKCoordinateSpanMake(0.02, 0.02)
+    }
+    
+    func configureMapView() {
+        
+    }
+    func configureSearchView() {
+        let searchController = UISearchController(searchResultsController: resultsController)
+        searchController.searchResultsUpdater = resultsController
+    }
+}
--- Podfile
+++ Podfile
@@ -1,21 +1,16 @@
 # Uncomment the next line to define a global platform for your project
 platform :ios, '10.0'
-workspace 'DawnPlusWorkSpace'
 
-target 'TrafficAlarmClock' do
+target 'DawnPlus' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
 
-  # Pods for TrafficAlarmClock
+  # Pods for DawnPlus
   pod 'DZNEmptyDataSet'
   pod 'MGSwipeTableCell'
+  pod 'SnapKit', '~> 3.2.0'
 
-  target 'TrafficAlarmClockTests' do
-    inherit! :search_paths
-    # Pods for testing
-  end
-
-  target 'TrafficAlarmClockUITests' do
+  target 'DawnPlusTests' do
     inherit! :search_paths
     # Pods for testing
   end
