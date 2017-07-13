0ddceea48bc5b172cae579f89511b13ccb527ab1
Xinke Chen <xinkecf35@gmail.com>
2017-07-13 01:14:26 -0400

    Rewriting SetAddressController to programmatically layout views

--- DawnPlus/AddressResultViewController.swift
+++ DawnPlus/AddressResultViewController.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+import SetAddressViewController
 
 class AddressResultViewController: UITableViewController{
     override func viewDidLoad() {
---
+++ DawnPlus/SwiftClasses/SetWorkAddressViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SetWorkAddressViewController.swift
+//  DawnPlus
+//
+//  Created by Xinke Chen on 2017-07-01.
+//  Copyright © 2017 Xinke Chen. All rights reserved.
+//
+
+import Foundation
