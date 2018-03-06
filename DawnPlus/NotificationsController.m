8bd49b73300b60e21e2bedd292c3ad3ec4c20351
Xinke Chen <xinkecf35@gmail.com>
2018-03-05 20:40:58 -0500

    Added stub for NotificationsController

---
+++ DawnPlus/NotificationsController.m
@@ -0,0 +1,17 @@
+//
+//  NotificationsController.m
+//  DawnPlus
+//
+//  Created by Xinke Chen on 2018-03-05.
+//  Copyright © 2018 Xinke Chen. All rights reserved.
+//
+
+#import "NotificationsController.h"
+
+@interface NotificationsController()
+
+@end
+
+@implementation NotificationsController
+
+@end
